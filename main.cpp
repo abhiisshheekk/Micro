fecc84db4c3f734216715958b48191cb32284d3c
abhiisshheekk <abhiisshheekk@gmail.com>
2021-03-02 20:18:33 +0530

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string>
+#include <vector>
 #include "parser.h"
+#include "headers/symbolTableStack.hpp"
 
+
+extern SymbolTableStack *tableStack;
 int yylex();
 int yyparse();
 void yyerror(char const *err){
-    printf("Not accepted\n");
+    // printf("Not accepted\n");
 };
 
 int main(int argc, char* argv[]) {
@@ -21,12 +26,13 @@ int main(int argc, char* argv[]) {
             yyin = fopen(argv[1], "r");
             // yyset_in(yyin);
 			retval = yyparse();
+            tableStack->printStack();
             fclose(yyin);
             // yylex();
         }
     }
-    if(retval == 0)
-	    printf("Accepted\n");
+    // if(retval == 0)
+	    // printf("Accepted\n");
     
     return 0;
 }
